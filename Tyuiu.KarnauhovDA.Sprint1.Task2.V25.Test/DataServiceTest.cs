901603fd08e1c78bf68a3db85a03812610f5cad6
Tyuiu.KarnauhovDA@outlook.com <hokagekakraikage@gmail.com>
2023-09-21 20:57:35 +0300

    Task2

---
+++ Tyuiu.KarnauhovDA.Sprint1.Task2.V25.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KarnauhovDA.Sprint1.Task2.V25.Lib;
+namespace Tyuiu.KarnauhovDA.Sprint1.Task2.V25.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 2;
+            var res = ds.ConvertRadsToDegrees(x);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ Tyuiu.KarnauhovDA.Sprint1.Task2.V25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KarnauhovDA.Sprint1.Task2.V25.Lib;
+namespace Tyuiu.KarnauhovDA.Sprint1.Task2.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Угол числа X = " + ds.ConvertRadsToDegrees(x));
+
+            Console.ReadLine();
+        }
+    }
+}
